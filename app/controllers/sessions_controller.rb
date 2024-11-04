201a0f1c4b00876f15c27236a3fd3a9470ac48d8
Hans B. K. Tognon <git@tobihans.space>
2024-11-05 00:47:55 +0100

    fix: do not signin if session is active already

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,9 @@ class SessionsController < ApplicationController
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
+    if authenticated?
+      redirect_to after_authentication_url
+    end
   end
 
   def create
