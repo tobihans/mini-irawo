3fa62e15fd5003a09395943966d732b98bec9f1f
Hans B. K. Tognon <git@tobihans.space>
2024-11-04 23:07:08 +0100

    feat(auth): lift simple auth scaffold from v8.1
    This commit introduces the auth scaffold added and plaaned to be release
    with RoR 8.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_04_182345) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_04_200349) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,7 +56,25 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_04_182345) do
     t.index ["category_id"], name: "index_resources_on_category_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "resources", "categories"
+  add_foreign_key "sessions", "users"
 end
