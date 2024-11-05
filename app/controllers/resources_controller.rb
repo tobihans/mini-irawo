82ec35d51d2793dd846e15554abf738cda106ac0
Hans B. K. Tognon <git@tobihans.space>
2024-11-05 13:47:31 +0100

    wip: display resources

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,7 +2,7 @@ class ResourcesController < ApplicationController
   allow_unauthenticated_access only: %i[ index show ]
 
   def index
-    @resources = Resource.all
+    @resources = Resource.order(created_at: :desc)
   end
 
   def show
@@ -26,12 +26,24 @@ class ResourcesController < ApplicationController
   end
 
   def edit
+    @resource = Resource.find(params[:id])
   end
 
   def update
+    @resource = Resource.find(params[:id])
+
+    if @resource.update(resource_params)
+      redirect_to @resource
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @resource = Resource.find(params[:id])
+    @resource.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
