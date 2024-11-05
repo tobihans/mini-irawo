2a0c2383563770a319fa04164588e665bba3f304
Hans B. K. Tognon <git@tobihans.space>
2024-11-05 09:33:43 +0100

    chore(dev): auto annotate models to ease fields lkp

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: resources
+#
+#  id          :integer          not null, primary key
+#  description :string
+#  kind        :string
+#  name        :string
+#  uri         :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :integer          not null
+#
+# Indexes
+#
+#  index_resources_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  category_id  (category_id => categories.id)
+#
 class Resource < ApplicationRecord
   belongs_to :category
   has_one_attached :image
